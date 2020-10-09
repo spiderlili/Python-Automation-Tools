c4461aef4b1eeac6e4d8ae50bb0fe87784bc6447
Jing <jtan1993@gmail.com>
2020-10-09 11:55:23 +0100

    Update SVGValidator.py

--- SVGValidator/SVGValidator.py
+++ SVGValidator/SVGValidator.py
@@ -1,1 +1,15 @@
-fileName = ''
+# validate SVG files so they can be converted by MSDF generator
+# from pip._vendor.pep517.compat import FileNotFoundError
+
+fileName = 'F1M_ClubIcons-13.svg'
+try:
+    with open(fileName) as svgFileObj:
+        svgContent = svgFileObj.read()
+except FileNotFoundError:
+    errorMsg = "sorry, the file: " + fileName + "cannot be found"
+    print (errorMsg)
+else:
+    words = svgContent.split()
+    numberOfWords = len(words)
+    print(numberOfWords)
+    
