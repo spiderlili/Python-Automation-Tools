36b835b5939ce60f97df9467080e25257f7bd8e4
Jing <jtan1993@gmail.com>
2020-10-10 15:06:52 +0100

    wordcount

--- TextAnalyser/textAnalyser.py
+++ TextAnalyser/textAnalyser.py
@@ -1,11 +1,18 @@
-filename = 'frankenstein.txt'
-try:
-    with open(filename) as fileObj
-        fileContent = fileObj.read()
-except IOError:
-    fileNotFoundMsg = "Sorry, the file: " + filename + "cannot be found"
-    print fileNotFoundMsg
-else:
-    wordsInFile = fileContent.split()
-    numberOfWords = len(wordsInFile)
-    print("The file: " + filename + " has approximately: " + str(numberOfWords) + " words.")
+def textAnalyser(filename):
+    """
+    Count the number of words in a file
+    """
+    try:
+        with open(filename) as fileObj:
+            fileContent = fileObj.read()
+    except IOError:
+        fileNotFoundMsg = "Sorry, the file: " + filename + "cannot be found"
+        print fileNotFoundMsg
+    else:
+        wordsInFile = fileContent.split()
+        numberOfWords = len(wordsInFile)
+        print("The file: " + filename + " has approximately: " + str(numberOfWords) + " words.")
+
+filenames = ['frankenstein.txt', 'aliceInWonderland.txt']
+for filename in filenames:
+    textAnalyser(filename)
