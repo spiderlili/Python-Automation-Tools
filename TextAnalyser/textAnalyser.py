41baa16491647d0ec6648e6ae2201ffa73469939
Jing <jtan1993@gmail.com>
2020-10-10 14:55:44 +0100

    add config

--- TextAnalyser/textAnalyser.py
+++ TextAnalyser/textAnalyser.py
@@ -1,1 +1,11 @@
 filename = 'frankenstein.txt'
+try:
+    with open(filename) as fileObj
+        fileContent = fileObj.read()
+except IOError:
+    fileNotFoundMsg = "Sorry, the file: " + filename + "cannot be found"
+    print fileNotFoundMsg
+else:
+    wordsInFile = fileContent.split()
+    numberOfWords = len(wordsInFile)
+    print("The file: " + filename + " has approximately: " + str(numberOfWords) + " words.")
