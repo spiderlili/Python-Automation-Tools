948595a04a067f3e49408df21763512999f5ed2a
Jing <jtan1993@gmail.com>
2020-10-11 22:16:57 +0100

    fixedDebug

--- SVGValidator/SVGSourceFilesCheck/SVGToMSDFChecker.py
+++ SVGValidator/SVGSourceFilesCheck/SVGToMSDFChecker.py
@@ -1,26 +1,29 @@
+polygonStr = 'polygon'
+pointsStr = 'points'
+pathStr = 'path'
+
 def SVGToMSDFAnalyser(filename):
     """
     Count the number of words in a file
     """
-    polygonStr = 'polygon'
-    pointsStr = 'points'
-    pathStr = 'path'
-
     try:
-        with open(filename) as fileObj:
-            fileContent = fileObj.readlines()
-        for line in fileContent:
-            pathCount = line.count(pathStr)
-            polygonCount = line.count(polygonStr)
-            print("The file: " + filename + " has: " + str(pathCount) + " paths.")
+        with open(filename) as svgFileObj:
+            fileContentLines = svgFileObj.readlines()
+
     except IOError:
         fileNotFoundMsg = "Sorry, the file: " + filename + "cannot be found"
         print fileNotFoundMsg
+
     else:
-        print "analysis finished!"
+        for line in fileContentLines:
+            pathCount = line.count(pathStr)
+            polygonCount = line.count(polygonStr)
+            print("The file: " + filename + " has: " + str(pathCount) + " paths.")
+            print("The file: " + filename + " has: " + str(polygonCount) + " polygons.")
         # wordsInFile = fileContent.split()
         # numberOfWords = len(wordsInFile)
 
 filenames = ['F1M_ClubIcons-13.svg', 'F1M_ClubIcons-14.svg']
 for filename in filenames:
     SVGToMSDFAnalyser(filename)
+print "analysis finished!"
