36f141fb4691f3ade534eb305e55d76bcea0c595
Jing Chun Tan <jtan1993@gmail.com>
2024-01-23 16:09:51 +0000

    Update qt_standalone_maya_example.py

--- Python-in-production/qt_standalone_maya_example.py
+++ Python-in-production/qt_standalone_maya_example.py
@@ -1,8 +1,17 @@
 from PySide6 import QtWidgets
-from shiboken2 import QtWidgets
+from shiboken2 import wrapInstance
+
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object: gets the main Maya window, parent the standalone window to Maya 
+    """
+    main_window_ptr = omui.MQTUtil.mainWindow()
+    return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
 
 # Window
 class StandaloneWindow(QtWidgets.QWidget):
+    wnd_instance = None
+
     def __init__(self):
         super(StandaloneWindow, self).__init__(parent=None)
         self.setWindowTitle("Standalone App")
@@ -10,8 +19,31 @@ class StandaloneWindow(QtWidgets.QWidget):
         self.close_btn = QtWidgets.QPushButton("Close", self)
         self.close_btn.clicked.connect(self.close)
 
-# Main Qt Application
+    # Toggles the window visibility: allows the script to be run from a shelf button in production
+    @classmethod 
+    def display(cls):
+        if not cls.wnd_instance:
+            cls.wnd_instance = StandaloneWindow()
+
+        if cls.wnd_instance.isHidden():
+            cls.wnd_instance.show()
+        else:
+            cls.wnd_instance.raise_()
+            cls.wnd_instance.activateWindow()
+
+# Main Qt Application: delete the dialogue each time the code is executed. 
+# Python environment is persistent between Maya sessions: PySide2 dialogues need to be explicitly deleted to pick up code changes
 if __name__ == "__main__":
+    try:
+        test_dialog.close()
+        test_dialog.deleteLater()
+
+    except:
+        pass
+
+    test_dialog = StandaloneWindow()
+    test_dialog.show()
+
     app = QtWidgets.QApplication(sys.argv)
     window = StandaloneWindow()
     window.show()
