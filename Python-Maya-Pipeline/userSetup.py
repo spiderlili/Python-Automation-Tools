6d2ff4cd64a04c0f2cf25a3d2e8015ebf1925683
Jing Chun Tan <jtan1993@gmail.com>
2024-03-11 14:04:27 +0000

    Update userSetup.py

--- Python-Maya-Pipeline/userSetup.py
+++ Python-Maya-Pipeline/userSetup.py
@@ -1,2 +1,5 @@
 import inspect
 import os
+
+user_setup_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+print("Reading userSetup.py from {0}".format(user_setup_path))
