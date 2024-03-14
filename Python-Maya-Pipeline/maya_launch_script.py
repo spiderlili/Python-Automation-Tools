f70d3b172d0164ad89d9421ccfac0d4f77f16744
Jing Chun Tan <jtan1993@gmail.com>
2024-03-14 09:57:36 +0000

    launch

--- Python-Maya-Pipeline/maya_launch_script.py
+++ Python-Maya-Pipeline/maya_launch_script.py
@@ -4,3 +4,9 @@ import subprocess
 MAYA_PATH = "/Applications/Autodesk/maya2024/Maya.app" # Mac
 
 # Launch Maya from the default environment
+# May need to give executable permission for this script to run
+maya_args = [MAYA_PATH]
+# subprocess.call is blocking - it waits for the process to exist (in this case Maya), before continuing to run the script.
+# subprocess.call(maya_args) 
+# subprocess.Popen is non-blocking - needs to be run through terminal, otherwise it ends immediately after launching Maya which also ends the Maya process
+subprocess.Popen(maya_args)
