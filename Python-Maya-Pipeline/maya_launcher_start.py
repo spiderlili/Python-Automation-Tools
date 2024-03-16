eaa601dc289b5937a60015cbec1cdc340b6471ea
Jing Chun Tan <jtan1993@gmail.com>
2024-03-16 21:47:56 +0000

    add launcher qt

--- Python-Maya-Pipeline/maya_launcher_start.py
+++ Python-Maya-Pipeline/maya_launcher_start.py
@@ -2,9 +2,10 @@ import os
 import subprocess
 import sys
 
-from PySide2 import QtCore
-from PySide2 import QtGui
-from PySide2 import QtWidgets
+# PySide6 with Qt6 supports version of python >=3.7 and <3.12
+from PySide6 import QtCore
+from PySide6 import QtGui
+from PySide6 import QtWidgets
 
 
 class ApplicationButtonWdg(QtWidgets.QWidget):
@@ -58,6 +59,7 @@ class ApplicationButtonWdg(QtWidgets.QWidget):
 
 class MayaLauncher(QtWidgets.QWidget):
 
+    # TODO: Add switch situations for Windows, Mac OS / Linux
     MAYA_2022_PATH = "C:/Program Files/Autodesk/Maya2022/bin/maya.exe"
     MAYA_2023_PATH = "C:/Program Files/Autodesk/Maya2023/bin/maya.exe"
 
@@ -108,10 +110,11 @@ class MayaLauncher(QtWidgets.QWidget):
         self.maya_2022_btn.clicked.connect(self.open_application)
         self.maya_2023_btn.clicked.connect(self.open_application)
 
+    # Application path is stored in the button instance & passed as part of a signal slot connection
     def open_application(self, application_path):
         print("Opening Application: {0}".format(application_path))
 
-        selected_env_path = ""
+        selected_env_path = "" # Base on the user's environment selection (stable / development)
         maya_args = []
 
         selected_env = self.env_select_cmb.currentText()
@@ -120,19 +123,33 @@ class MayaLauncher(QtWidgets.QWidget):
         elif selected_env == "Dev":
             selected_env_path = "D:/PythonDev"
 
+        # Load selected scene when Maya loads: either the empty scene or file path
         scene_path = self.scene_select_cmb.currentText()
         if scene_path != MayaLauncher.EMPTY_SCENE:
             maya_args = ["-file", scene_path]
 
         self.open_application_python(application_path, selected_env_path, maya_args)
+        # self.open_application_qt(application_path, selected_env_path, maya_args)
 
     def open_application_python(self, application_path, selected_env_path, maya_args):
         maya_env = os.environ.copy()
         maya_env["PYTHONPATH"] = selected_env_path
 
+        # When using Popen & passing in the args for the application: the executable path needs to be the 1st arg in that list
         maya_args.insert(0, application_path)
         subprocess.Popen(maya_args, env=maya_env)
 
+    # Qt-based alternative to the subprocess module - leverage the Qt process class. 
+    # advantages in Qt applications through the use of signals & slots despite similarity in setup & functionality to subprocess
+    def open_application_qt(self, application_path, selected_env_path, maya_args):
+        process_environment = QtCore.QProcessEnvironment.systemEnvironment()
+        process_environment.insert("PYTHONPATH", selected_env_path)
+        process = QtCore.QProcess(self)
+        # Provide the environment, application path, Maya-specific args
+        process.setProcessEnvironment(process_environment)
+        process.setProgram(application_path)
+        process.setArguments(maya_args)
+        process.startDetached() # Make sure the process does not block script execution
 
 if __name__ == "__main__":
     # Create the main Qt application
