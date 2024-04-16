cd7f9ec6d8fad9dd1a8a00f01247d8a67710af3a
Jing Chun Tan <jtan1993@gmail.com>
2024-04-16 16:00:22 +0100

    Update hello_world_qt.py

--- Qt_Python_Maya/hello_world_qt.py
+++ Qt_Python_Maya/hello_world_qt.py
@@ -1,2 +1,17 @@
 # > Maya 2025: use PySide6
-from PySide2 import QtWidgets 
+import maya.cmds as cmds
+
+try:
+    from PySide2 import QtWidgets # Qt5(<= Maya 2025)
+except:
+    from PySide6 import QtWidgets # Qt6 (> Maya 2025)
+
+class HelloQtWindow(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Hello Qt")
+
+
+if __name__ == "__main__":
+    window = HelloQtWindow()
+    window.show()
