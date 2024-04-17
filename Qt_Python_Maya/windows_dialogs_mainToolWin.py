43b4b2d7ac8d88e01b5703d6ff339402aa1442c6
Jing Chun Tan <jtan1993@gmail.com>
2024-04-17 16:38:20 +0100

    Update windows_dialogs_mainToolWin.py

--- Qt_Python_Maya/windows_dialogs_mainToolWin.py
+++ Qt_Python_Maya/windows_dialogs_mainToolWin.py
@@ -1,15 +1,29 @@
-# > Maya 2025: use PySide6
 import maya.cmds as cmds
+import maya.OpenMayaUI as omui
 
 try:
     # Qt5(<= Maya 2025)
     from PySide2 import QtCore
     from PySide2 import QtWidgets 
+    from shiboken2 import wrapInstance
 except:
     # Qt6 (> Maya 2025)
     from PySide6 import QtCore
     from PySide6 import QtWidgets 
+    from shiboken6 import wrapInstance
+
+# convert the C++ pointer so that it’s a valid python object
+def maya_main_window():
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(int(main_window_ptr), QtWidgets.QWidget)
 
 class MainToolWindow(QtWidgets.QDialog):
-    def __init__(self, parent=None):
+    # Reparent to Maya's main window to always display on top even when not on focus
+    def __init__(self, parent=maya_main_window): 
         super().__init__(parent)
+        self.setWindowTitle("Windows Dialogs")
+        self.setMinimumSize(400, 300)
+
+if __name__ == "__main__":
+    win = MainToolWindow()
+    win.show()
