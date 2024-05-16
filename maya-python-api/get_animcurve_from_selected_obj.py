b6a50987b6352a7ceb3413f36c29dfede6a5f6f6
Jing Chun Tan <jtan1993@gmail.com>
2024-05-16 19:57:04 +0100

    Update get_animcurve_from_selected_obj.py

--- maya-python-api/get_animcurve_from_selected_obj.py
+++ maya-python-api/get_animcurve_from_selected_obj.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
         anim_curve_fn = get_anim_curve(obj, "translateY")
         if anim_curve_fn:
             set_value(anim_curve_fn, 1, 3)
-            set_time(anim_curve_fn, 0, get_current_time)
+            set_time(anim_curve_fn, 0, get_current_time())
 
         if add_key(obj, "translateX") is None:
             remove_key(obj, "translateX")
