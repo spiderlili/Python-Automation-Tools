1bfb17dd5bfc709ccb3da2b4e7d48958b61253b4
Jing Chun Tan <jtan1993@gmail.com>
2024-06-03 16:28:26 +0100

    Update openImportRefTool.py

--- Qt_Python_Maya/open_import_tool/openImportRefTool.py
+++ Qt_Python_Maya/open_import_tool/openImportRefTool.py
@@ -35,10 +35,43 @@ class OpenImportDialog(QtWidgets.QDialog):
         self.create_connections()
 
     def create_widgets(self):
-        pass
+        self.filepath_le = QtWidgets.QLineEdit()
+        self.select_file_path_btn = QtWidgets.QPushButton()
+        # Show a Maya-provided folder icon that's commonly used for file open operations using Qt resource 
+        self.select_file_path_btn.setIcon(QtGui.QIcon(":fileOpen.png"))
+
+        self.open_rb = QtWidgets.QRadioButton("Open")
+        self.open_rb.setChecked(True)
+
+        self.import_rb = QtWidgets.QRadioButton("Import")
+        self.reference_rb = QtWidgets.QRadioButton("Reference")        
+        self.force_cb = QtWidgets.QCheckBox("Force")
+        self.apply_btn = QtWidgets.QPushButton("Apply")
+        self.close_btn = QtWidgets.QPushButton("Close")
 
     def create_layout(self):
-        pass
+        file_path_layout = QtWidgets.QHBoxLayout()
+        file_path_layout.addWidget(self.filepath_le)
+        file_path_layout.addWidget(self.select_file_path_btn)
+
+        radio_btn_layout = QtWidgets.QHBoxLayout()
+        radio_btn_layout.addWidget(self.open_rb)
+        radio_btn_layout.addWidget(self.import_rb)
+        radio_btn_layout.addWidget(self.reference_rb)
+
+        form_layout = QtWidgets.QFormLayout()
+        form_layout.addRow("File Path: ", file_path_layout)
+        form_layout.addRow("", radio_btn_layout)
+        form_layout.addRow("", self.force_cb)
+
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.apply_btn)
+        button_layout.addWidget(self.close_btn)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(button_layout)
 
     def create_connections(self):
         pass
